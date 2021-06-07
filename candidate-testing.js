8fe2615505998b4c743ef55271f78822070acfe4
Jackson Hall <>
2021-06-07 20:48:00 +0000

    a lottt

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,5 +1,6 @@
 const input = require('readline-sync');
 
+
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
@@ -10,51 +11,52 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = '';
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5+3)/2*10=? ", "Given the array [8, 'Orbit', 'Trajectory', 45]. what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers = [];
-
+let correctAnswersB = ["sally ride", "True", "40", "trajectory", "3"];
+let candidateAnswers = [].map(c=> v.toLowerCase());
+let result = [];
+let ca = 0;
+let grade = (ca/5)*100
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
     candidateName = input.question("What is your name? ");
     
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  // candidateAnswer = input.question(question);
-
-
   for (let i = 0; i < questions.length; i++) {
       candidateAnswers.push(input.question(questions[i]));
   }
+}
 
-
-
+function forceLower(candidateAnswers) {
+  for (let i = 0; i < candidateAnswers.length; i++) {
+    candidateAnswers[i].toLowerCase();
+  } 
 }
 
 function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-   /* if (candidateAnswer === correctAnswer) {
-        console.log("Your answer is correct.");
-    } else {
-        console.log("Your answer is incorrect.");
-    } */
-
      for (let i = 0; i < candidateAnswers.length; i++) {
-        if (candidateAnswers[i] === correctAnswers[i]) {
-            console.log("correct");
+        if (candidateAnswers[i] === correctAnswersB[i]) {
+          result.push("correct")
         } else {
-            console.log("incorrect.");
+          result.push("incorrect")
         }
-      }
-
-
-  let grade;
-  
-
-  return grade;
+     }
 }
+
+    
+
+function gradeQuiz(result) {
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] === "correct") {
+    ca = ca + 1
+    }   
+  }
+}
+
+
+// function gradeQuiz()
+
 
 function runProgram() {
   askForName();
@@ -64,6 +66,8 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
@@ -77,4 +81,5 @@ module.exports = {
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
-};
+}
+
